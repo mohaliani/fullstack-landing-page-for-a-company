82f6e98cb43d590be3d5447e7f823102e251ebec
Mohamed Aliani <mohamdaliani0419@gmail.com>
2023-10-06 16:55:29 +0100

    Add .gitignore file

---
+++ client/src/data/slider-data.js
@@ -0,0 +1,33 @@
+const imageslider = [
+  {
+    url: "image-1.jpg",
+  },
+  {
+    url: "image-2.jpg",
+  },
+  {
+    url: "image-3.jpg",
+  },
+  {
+    url: "image-4.jpg",
+  },
+  {
+    url: "image-5.jpg",
+  },
+  {
+    url: "image-6.jpg",
+  },
+  {
+    url: "image-7.jpg",
+  },
+  {
+    url: "image-8.jpg",
+  },
+  {
+    url: "image-9.jpg",
+  },
+  {
+    url: "image-10.jpg",
+  },
+];
+export default imageslider;
